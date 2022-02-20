5bffc3da73acca70371481a54874efe9c3f9ecb7
Ryan Uzumaki <ryandx0de@outlook.com>
2022-02-20 16:06:37 +0800

    fianl version

--- Process.hpp
+++ Process.hpp
@@ -30,9 +30,9 @@ using namespace cv;
 class Process {
 public:
 	double get_distance(int W, int P);
-	//Point frame_1_center_point(Mat& image);
+	void predict(Mat& image, Mat& image_temp, int avgX_temp, int avgY_temp);
 	string Convert(float Num);
-	void object_recognition(Mat& image);
+	void object_recognition(Mat& image, Mat& image_temp_1);
 };
 
 template<class ForwardIterator>
@@ -64,7 +64,7 @@ double Process::get_distance(int W, int P) {
 }
 
 
-void Process::object_recognition(Mat& image) {
+void Process::object_recognition(Mat& image, Mat& image_temp_1) {
 	Process pr;
 	int known_W = 9;
 	int known_P = 510;
@@ -91,8 +91,8 @@ void Process::object_recognition(Mat& image) {
 	size_t maxIndex = distance(area.begin(), max_element(area.begin(), area.end()));
 	Rect ret_1 = boundingRect(contours[maxIndex]);
 	int avgX, avgY;
-	avgX = (ret_1.x + ret_1.width) / 2;//x-axis middle point
-	avgY = (ret_1.y + ret_1.height) / 2;//y-axis middle point
+	avgX = ret_1.x;
+	avgY = ret_1.y;
 	for (int i = 0; i < contours.size(); i++) {
 		//for (int j = 0; j < contours[i].size(); j++) {
 		//	Point P = Point(contours[i][j].x, contours[i][j].y);
@@ -103,9 +103,52 @@ void Process::object_recognition(Mat& image) {
 		rectangle(image, box, Scalar(0, 0, 255), 2, 8, 0);
 		drawContours(image, contours, maxIndex, Scalar(0, 255, 0), 2, 8, hierarchy);
 	}
+	pr.predict(image, image_temp_1, avgX, avgY);
 	double dist = pr.get_distance(known_W, ret_1.width);
 	string dist_str = pr.Convert(dist);
 	putText(image, "Distance:" + dist_str + "cm", Point(50, 50), FONT_HERSHEY_COMPLEX, 1, Scalar(50, 250, 50), 2, 8);
 	namedWindow("detected", WINDOW_FREERATIO);
 	imshow("detected", image);
+}
+
+void Process::predict(Mat& image, Mat& image_temp, int avgX_temp, int avgY_temp) {
+	Mat frame_temp;
+	frame_temp = image_temp.clone();
+	Process pr;
+	int known_W = 9;
+	int known_P = 510;
+	Mat temp = Mat::zeros(image_temp.size(), image_temp.type());
+	Mat m = Mat::zeros(image_temp.size(), image_temp.type());
+	addWeighted(image_temp, 0.19, m, 0.0, 0, temp);
+	Mat dst;
+	bilateralFilter(temp, dst, 5, 20, 20);
+	Mat m_ResImg;
+	cvtColor(dst, m_ResImg, COLOR_BGR2HSV);
+	Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
+	erode(m_ResImg, m_ResImg, element);//进行腐蚀操作
+	erode(m_ResImg, m_ResImg, element);//进行腐蚀操作
+	erode(m_ResImg, m_ResImg, element);//进行腐蚀操作
+	Mat mask;
+	inRange(m_ResImg, Scalar(100, 43, 46), Scalar(124, 255, 255), mask);
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+	findContours(mask, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point());
+	vector<double> area;
+	for (int i = 0; i < contours.size(); i++) {
+		area.push_back(contourArea(contours[i]));
+	}
+	size_t maxIndex = distance(area.begin(), max_element(area.begin(), area.end()));
+	Rect ret_1 = boundingRect(contours[maxIndex]);
+	int avgX, avgY;
+	int avgX_mean, avgY_mean;
+	avgX = ret_1.x;
+	avgY = ret_1.y;
+	avgX_mean = (avgX + avgX_temp) / 2;
+	avgY_mean = (avgY + avgY_temp) / 2;
+	Rect ret_2(avgX_mean, avgY_mean, ret_1.width, ret_1.height);
+	Rect box(ret_2.x, ret_2.y, ret_2.width, ret_2.height);
+	rectangle(image, box, Scalar(255, 0, 0), 2, 8, 0);
+	/*for (int i = 0; i < contours.size(); i++) {
+		drawContours(image, contours, maxIndex, Scalar(0, 255, 0), 2, 8, hierarchy);
+	}*/
 }
