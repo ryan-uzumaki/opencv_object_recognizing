41976a256f5d3d342a74d35ceead477b372409b7
Ryan Uzumaki <ryandx0de@outlook.com>
2022-02-20 17:54:18 +0800

    final version

--- main.cpp
+++ main.cpp
@@ -47,36 +47,3 @@ int main() {
 
 
 
-void detect_object(Mat& imageSource) {
-	//imshow("Source Image", imageSource);
-	Mat image = Mat::zeros(imageSource.size(), imageSource.type());
-	image = imageSource.clone();
-	//GaussianBlur(imageSource, image, Size(3, 3), 0);
-	Canny(image, image, 50, 100);
-	vector<vector<Point>> contours;
-	vector<Vec4i> hierarchy;
-	findContours(image, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point());
-	Mat imageContours = Mat::zeros(image.size(), CV_8UC1);
-	Mat Contours = Mat::zeros(image.size(), CV_8UC1);  //绘制
-	for (int i = 0; i < contours.size(); i++) {
-		//contours[i]代表的是第i个轮廓，contours[i].size()代表的是第i个轮廓上所有的像素点数
-		for (int j = 0; j < contours[i].size(); j++) {
-			//绘制出contours向量内所有的像素点
-			Point P = Point(contours[i][j].x, contours[i][j].y);
-			Contours.at<uchar>(P) = 255;
-		}
-
-		//输出hierarchy向量内容
-		/*char ch[256];
-		sprintf_s(ch, "%d", i);
-		string str = ch;
-		cout << "向量hierarchy的第" << str << " 个元素内容为：" << endl << hierarchy[i] << endl << endl;*/
-
-		//绘制轮廓
-		drawContours(imageContours, contours, i, Scalar(255), 1, 8, hierarchy);
-	}
-	imshow("Contours Image", imageContours); //轮廓
-	//imshow("Point of Contours", Contours);   //向量contours内保存的所有轮廓点集
-	waitKey(0);
-}
-
